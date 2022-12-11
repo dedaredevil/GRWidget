eef14d3f52023cc372e188f8a2b030ecdd65f130
Brandon Soares <brandonjsoares@gmail.com>
2022-12-11 12:30:46 -0500

    Only shows reviews above assigned lowest score

--- src/script.js
+++ src/script.js
@@ -29,64 +29,68 @@ function initMap() {
 }
 
 function createCard(place, id) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  document.getElementById("section").append(card);
+  const lowestReviewScore = 3;
 
-  const mediaContent = document.createElement("div");
-  mediaContent.classList.add("media-content");
-  card.append(mediaContent);
+  if (place.reviews[id].rating >= lowestReviewScore) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    document.getElementById("section").append(card);
 
-  const reviewPhoto = document.createElement("img");
-  reviewPhoto.classList.add("review-photo");
-  reviewPhoto.src = place.reviews[id].profile_photo_url;
-  setAttributes(reviewPhoto, {
-    alt: "Review Photo",
-    style: "width:100%",
-  });
-  mediaContent.append(reviewPhoto);
+    const mediaContent = document.createElement("div");
+    mediaContent.classList.add("media-content");
+    card.append(mediaContent);
 
-  const reviewName = document.createElement("div");
-  reviewName.classList.add("title", "is-4");
-  reviewName.innerHTML = place.reviews[id].author_name;
-  mediaContent.append(reviewName);
-
-  const starsContainer = document.createElement("div");
-  starsContainer.classList.add("content-container");
-  mediaContent.append(starsContainer);
-
-  for (let i = 0; i < place.reviews[id].rating; i++) {
-    const starElement = document.createElement("img");
-    setAttributes(starElement, {
-      src: "/images/star.jpg",
-      alt: "Star",
-      height: "15",
-      width: "16",
+    const reviewPhoto = document.createElement("img");
+    reviewPhoto.classList.add("review-photo");
+    reviewPhoto.src = place.reviews[id].profile_photo_url;
+    setAttributes(reviewPhoto, {
+      alt: "Review Photo",
+      style: "width:100%",
     });
-    starsContainer.append(starElement);
+    mediaContent.append(reviewPhoto);
+
+    const reviewName = document.createElement("div");
+    reviewName.classList.add("title", "is-4");
+    reviewName.innerHTML = place.reviews[id].author_name;
+    mediaContent.append(reviewName);
+
+    const starsContainer = document.createElement("div");
+    starsContainer.classList.add("content-container");
+    mediaContent.append(starsContainer);
+
+    for (let i = 0; i < place.reviews[id].rating; i++) {
+      const starElement = document.createElement("img");
+      setAttributes(starElement, {
+        src: "/images/star.jpg",
+        alt: "Star",
+        height: "15",
+        width: "16",
+      });
+      starsContainer.append(starElement);
+    }
+
+    const reviewText = document.createElement("div");
+    reviewText.classList.add("review-text");
+    reviewText.innerHTML = place.reviews[id].text;
+    mediaContent.append(reviewText);
+
+    const contentContainer = document.createElement("div");
+    contentContainer.classList.add("content-container");
+    mediaContent.append(contentContainer);
+
+    const placesLogo = document.createElement("img");
+    placesLogo.classList.add("places-logo");
+    setAttributes(placesLogo, {
+      src: "/images/places-logo.jpg",
+      alt: "Review Photo",
+    });
+    contentContainer.append(placesLogo);
+
+    const reviewTime = document.createElement("div");
+    reviewTime.classList.add("is-grey", "is-6");
+    reviewTime.innerHTML = place.reviews[id].relative_time_description;
+    contentContainer.append(reviewTime);
   }
-
-  const reviewText = document.createElement("div");
-  reviewText.classList.add("review-text");
-  reviewText.innerHTML = place.reviews[id].text;
-  mediaContent.append(reviewText);
-
-  const contentContainer = document.createElement("div");
-  contentContainer.classList.add("content-container");
-  mediaContent.append(contentContainer);
-
-  const placesLogo = document.createElement("img");
-  placesLogo.classList.add("places-logo");
-  setAttributes(placesLogo, {
-    src: "/images/places-logo.jpg",
-    alt: "Review Photo",
-  });
-  contentContainer.append(placesLogo);
-
-  const reviewTime = document.createElement("div");
-  reviewTime.classList.add("is-grey", "is-6");
-  reviewTime.innerHTML = place.reviews[id].relative_time_description;
-  contentContainer.append(reviewTime);
 }
 
 function setAttributes(el, attrs) {
