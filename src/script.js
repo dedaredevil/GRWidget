8d3e152522a3c4cc3af4b448c3346082c5c40b50
Brandon Soares <brandonjsoares@gmail.com>
2022-12-15 18:29:28 -0500

    Update container to be responsive w/ single column Single column layout will adjust size depending on the total review height.

--- src/script.js
+++ src/script.js
@@ -25,6 +25,7 @@ function initMap() {
         createCard(place, i);
       }
       createTitle(place);
+      resizeFlexbox();
     } else {
       console.log("PlacesServiceStatusError");
     }
@@ -37,22 +38,22 @@ function createTitle(place) {
     "user-ratings-total"
   ).innerHTML = `of ${place.user_ratings_total} reviews`;
 
-  const rating = place.rating;
+  const RATING = place.rating;
 
   switch (true) {
-    case rating >= 4.5:
+    case RATING >= 4.5:
       createStars(5);
       break;
-    case rating >= 3.5:
+    case RATING >= 3.5:
       createStars(4);
       break;
-    case rating >= 2.5:
+    case RATING >= 2.5:
       createStars(3);
       break;
-    case rating >= 1.5:
+    case RATING >= 1.5:
       createStars(2);
       break;
-    case rating >= 0.5:
+    case RATING >= 0.5:
       createStars(1);
       break;
     default:
@@ -61,65 +62,65 @@ function createTitle(place) {
 
 function createStars(count) {
   for (let i = 0; i < count; i++) {
-    const starElement = document.createElement("img");
-    starElement.classList.add("title__stars");
-    setAttributes(starElement, {
+    const STAR_ELEMENT = document.createElement("img");
+    STAR_ELEMENT.classList.add("title__stars");
+    setAttributes(STAR_ELEMENT, {
       src: "/images/star.jpg",
       alt: "Star",
     });
-    document.getElementById("title__card").append(starElement);
+    document.getElementById("title__card").append(STAR_ELEMENT);
   }
 }
 
 function createCard(place, id) {
-  const lowestReviewScore = 3;
+  const LOWEST_REVIEW_SCORE = 3;
 
-  if (place.reviews[id].rating >= lowestReviewScore) {
-    const card = document.createElement("a");
-    card.classList.add("card");
-    card.setAttribute("href", place.reviews[id].author_url);
-    document.getElementById("masonry__chopper").append(card);
+  if (place.reviews[id].rating >= LOWEST_REVIEW_SCORE) {
+    const CARD = document.createElement("a");
+    CARD.classList.add("card");
+    CARD.setAttribute("href", place.reviews[id].author_url);
+    document.getElementById("masonry__chopper").append(CARD);
 
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card__body");
-    card.append(cardBody);
+    const CARD_BODY = document.createElement("div");
+    CARD_BODY.classList.add("card__body");
+    CARD.append(CARD_BODY);
 
-    const reviewPhoto = document.createElement("img");
-    reviewPhoto.classList.add("card__profile-photo");
-    reviewPhoto.src = place.reviews[id].profile_photo_url;
-    setAttributes(reviewPhoto, {
+    const REVIEW_PHOTO = document.createElement("img");
+    REVIEW_PHOTO.classList.add("card__profile-photo");
+    REVIEW_PHOTO.src = place.reviews[id].profile_photo_url;
+    setAttributes(REVIEW_PHOTO, {
       alt: "Review Photo",
       style: "width:100%",
     });
-    cardBody.append(reviewPhoto);
+    CARD_BODY.append(REVIEW_PHOTO);
 
-    const reviewName = document.createElement("div");
-    reviewName.classList.add("card__author");
-    reviewName.innerHTML = place.reviews[id].author_name;
-    cardBody.append(reviewName);
+    const REVIEW_NAME = document.createElement("div");
+    REVIEW_NAME.classList.add("card__author");
+    REVIEW_NAME.innerHTML = place.reviews[id].author_name;
+    CARD_BODY.append(REVIEW_NAME);
 
-    const starsContainer = document.createElement("div");
-    starsContainer.classList.add("card__flex-container");
-    cardBody.append(starsContainer);
+    const STARS_CONTAINER = document.createElement("div");
+    STARS_CONTAINER.classList.add("card__flex-container");
+    CARD_BODY.append(STARS_CONTAINER);
 
     for (let i = 0; i < place.reviews[id].rating; i++) {
-      const starElement = document.createElement("img");
-      starElement.classList.add("card__star");
-      setAttributes(starElement, {
+      const STAR_ELEMENT = document.createElement("img");
+      STAR_ELEMENT.classList.add("card__star");
+      setAttributes(STAR_ELEMENT, {
         src: "/images/star.jpg",
         alt: "Star",
       });
-      starsContainer.append(starElement);
+      STARS_CONTAINER.append(STAR_ELEMENT);
     }
 
-    const reviewText = document.createElement("div");
-    reviewText.classList.add("card__review");
-    reviewText.innerHTML = place.reviews[id].text;
-    cardBody.append(reviewText);
+    const REVIEW_TEXT = document.createElement("div");
+    REVIEW_TEXT.classList.add("card__review");
+    REVIEW_TEXT.innerHTML = place.reviews[id].text;
+    CARD_BODY.append(REVIEW_TEXT);
 
-    const contentContainer = document.createElement("div");
-    contentContainer.classList.add("card__flex-container");
-    cardBody.append(contentContainer);
+    const CONTENT_CONTAINER = document.createElement("div");
+    CONTENT_CONTAINER.classList.add("card__flex-container");
+    CARD_BODY.append(CONTENT_CONTAINER);
 
     const placesLogo = document.createElement("img");
     placesLogo.classList.add("card__logo");
@@ -127,12 +128,12 @@ function createCard(place, id) {
       src: "/images/places-logo.jpg",
       alt: "Review Photo",
     });
-    contentContainer.append(placesLogo);
+    CONTENT_CONTAINER.append(placesLogo);
 
-    const reviewTime = document.createElement("div");
-    reviewTime.classList.add("card__time");
-    reviewTime.innerHTML = place.reviews[id].relative_time_description;
-    contentContainer.append(reviewTime);
+    const REVIEW_TIME = document.createElement("div");
+    REVIEW_TIME.classList.add("card__time");
+    REVIEW_TIME.innerHTML = place.reviews[id].relative_time_description;
+    CONTENT_CONTAINER.append(REVIEW_TIME);
   }
 }
 
@@ -141,3 +142,22 @@ function setAttributes(el, attrs) {
     el.setAttribute(key, attrs[key]);
   }
 }
+
+function resizeFlexbox() {
+  const MEDIA_QUERY_SINGLE = 768;
+  if (window.innerWidth < MEDIA_QUERY_SINGLE) {
+    const CARDS = document.getElementsByClassName("card");
+    let maxHeight = 0;
+    const OFFSET_HEIGHT = 150;
+    for (let i = 0; i < CARDS.length; i++) {
+      maxHeight += CARDS[i].clientHeight;
+    }
+    document.getElementById("masonry__chopper").style.height = `${
+      maxHeight + OFFSET_HEIGHT
+    }px`;
+  } else {
+    document.getElementById("masonry__chopper").style.height = null;
+  }
+}
+
+window.addEventListener("resize", resizeFlexbox);
